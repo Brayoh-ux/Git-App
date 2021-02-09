874ab447c306f45f71293c8c06ee72c486f690e1
BrianMbugua5693 <brianmbugua5693@gmail.com>
2021-02-09 11:45:35 +0300

    input from parent

--- src/app/repos/repos.component.ts
+++ src/app/repos/repos.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-repos',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./repos.component.css']
 })
 export class ReposComponent implements OnInit {
+
+  @Input() githubRepos: any[];
 
   constructor() { }
 
