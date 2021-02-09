87c1dbfa62efbb03f2f0e65cc5acee8f8af134c4
BrianMbugua5693 <brianmbugua5693@gmail.com>
2021-02-09 08:10:41 +0300

    add data part

---
+++ src/app/profile-card/profile-card.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-card',
+  templateUrl: './profile-card.component.html',
+  styleUrls: ['./profile-card.component.css']
+})
+export class ProfileCardComponent implements OnInit {
+
+  @Input() githubProfile: any;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
