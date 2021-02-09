5f6c431465c3134de833e19aebb47185bffa2705
BrianMbugua5693 <brianmbugua5693@gmail.com>
2021-02-09 08:12:28 +0300

    add form module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,30 @@ import { ProfileService } from './services/profile.service';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './profile/profile.component';
+import { ReposComponent } from './repos/repos.component';
+import { ProfileDataComponent } from './profile-data/profile-data.component';
+import { ProfileCardComponent } from './profile-card/profile-card.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProfileComponent
+    ProfileComponent,
+    ReposComponent,
+    ProfileDataComponent,
+    ProfileCardComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [ProfileService],
   bootstrap: [AppComponent]
